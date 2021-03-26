6e80f104cd573d9bc1d570c614e5424ba8acc396
Mbongeni Mavuso <mbongenimavuso@Mbongenis-MacBook-Air.local>
2021-03-26 16:33:30 +0200

    msc backup.

--- app/src/main/java/com/example/goaltrivia/splash_screen.kt
+++ app/src/main/java/com/example/goaltrivia/splash_screen.kt
@@ -2,10 +2,18 @@ package com.example.goaltrivia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class splash_screen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
     }
+
+    fun sendMessage(view: View) {
+        // Do something in response to button
+    }
+
+
 }
